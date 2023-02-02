53e01999c015f0c700a7dd380227173f94114b75
andreybyrsa <byrsa2004@bk.ru>
2023-02-02 21:53:06 +0300

    Добавил настраиваемые цвета в проект

---
+++ src/assets/styles/colors/index.js
@@ -0,0 +1,13 @@
+import colors from './export.scss';
+
+export default {
+  primary: colors.primary,
+  error: colors.error,
+  success: colors.success,
+  gray_0: colors.gray_0,
+  gray_20: colors.gray_20,
+  gray_40: colors.gray_40,
+  gray_60: colors.gray_60,
+  gray_80: colors.gray_80,
+  gray_1000: colors.gray_100,
+}
