8d3222fcba754731e9a4427c47fa54470bca1c1e
andreybyrsa <byrsa2004@bk.ru>
2023-02-03 22:15:48 +0300

    Сделал компонент Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import Typography from '../Typography/Typography';
 import colors from '../../assets/styles/colors';
 
 import './Header.scss';
+import TypographyVariantsTypes from "../Typography/TypographyVariants.types";
 
 function Header(
   className,
@@ -20,8 +21,15 @@ function Header(
         <Icon iconPath={IconsPaths.headerLogo} size={32} viewBox={32} color={colors.primary} />
         <Typography className="header__logo-title" color={colors.primary}>LifeTest</Typography>
       </div>
-      <div className="header__interface-tracker">
-
+      <div className="header__tracker">
+        <div className="header__tracker-task">
+          <Typography variant={TypographyVariantsTypes.Text_regular} color={colors.gray_100}>Сделать UI-kit</Typography>
+          <Icon iconPath={IconsPaths.stop} color={colors.primary} />
+        </div>
+        <div className="header__tracker-timer">
+          <Typography>Сегодня:</Typography>
+          <Typography variant={TypographyVariantsTypes.Time_regular}>0:20:42</Typography>
+        </div>
       </div>
     </div>
   );
