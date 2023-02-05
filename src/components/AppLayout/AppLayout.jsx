a69da69d56534dca13e13c119ea185a050a93393
andreybyrsa <byrsa2004@bk.ru>
2023-02-05 12:10:28 +0500

    Добавил приттиер в проект

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,52 +1,59 @@
-import React, { useMemo } from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import TaskInput from '../Inputs/TaskInput/TaskInput';
-import TaskBar from '../TaskBar/TaskBar';
-import Task from '../Task/Task';
+import React, { useEffect, useState } from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import TaskInput from '../Inputs/TaskInput/TaskInput'
+import TaskBar from '../TaskBar/TaskBar'
+import Task from '../Task/Task'
 
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 
-import './AppLayout.scss';
+import './AppLayout.scss'
 
-function AppLayout({
- className,
- contentClassName,
- taskBarClassName,
-}) {
+function AppLayout({ className, contentClassName, taskBarClassName }) {
+  const [currentTaskId, setCurrentTaskId] = useState(-1)
+  const todos = useSelector((state) => state.todosStore.todos)
 
-  const todos = useSelector(state => state.todosStore.todos);
+  useEffect(() => {
+    if (todos.length === 0) {
+      setCurrentTaskId((prevState) => prevState * -1)
+    }
+    if (todos.length > 0) {
+      setCurrentTaskId(0)
+    }
+  }, [todos])
 
-  const AppLayoutClassName = classNames(
-    'app-layout',
-    className,
-  );
-  const AppLayoutContentClassName = classNames(
-    'app-layout__content',
-    contentClassName,
-  );
-  const AppLayoutTaskBarClassName = classNames(
-    'app-layout__task-bar',
-    taskBarClassName,
-  );
+  const AppLayoutClassName = classNames('app-layout', className)
+  const AppLayoutContentClassName = classNames('app-layout__content', contentClassName)
+  const AppLayoutTaskBarClassName = classNames('app-layout__task-bar', taskBarClassName)
 
-  const taskBar = useMemo(() => {
-    return <TaskBar />
-  }, []);
+  const showCurrentTaskBar = (taskId) => {
+    const currentIndex = todos.findIndex((elem) => elem.id === taskId)
+    setCurrentTaskId(currentIndex)
+  }
 
   return (
     <div className={AppLayoutClassName}>
       <div className={AppLayoutContentClassName}>
         <TaskInput />
         {todos.map((elem) => (
-          <Task key={elem.id} task={elem} taskName={elem.todo} trackedTime={elem.trackedTime} />
+          <Task
+            key={elem.id}
+            onClick={() => showCurrentTaskBar(elem.id)}
+            task={elem}
+            taskName={elem.todo}
+            trackedTime={elem.trackedTime}
+          />
         ))}
       </div>
-      <div className={AppLayoutTaskBarClassName}>
-        {taskBar}
-      </div>
+      {todos.length > 0 && currentTaskId >= 0 ? (
+        <div className={AppLayoutTaskBarClassName}>
+          <TaskBar task={todos[currentTaskId]} />
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  );
+  )
 }
 
 AppLayout.defaultProps = {
@@ -54,13 +61,13 @@ AppLayout.defaultProps = {
   contentClassName: '',
   taskBarClassName: '',
   children: null,
-};
+}
 
 AppLayout.propTypes = {
   className: PropTypes.string,
   contentClassName: PropTypes.string,
   taskBarClassName: PropTypes.string,
   children: PropTypes.array,
-};
+}
 
-export default AppLayout;
+export default AppLayout
